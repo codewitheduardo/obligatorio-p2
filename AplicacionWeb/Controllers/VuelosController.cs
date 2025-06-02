a536b5f4e32e65c195313551760aae4f18be1988
Eduardo Monzón <xeriousuy@gmail.com>
2025-06-02 18:52:27 -0300

    Add project files.

---
+++ AplicacionWeb/Controllers/VuelosController.cs
@@ -0,0 +1,32 @@
+﻿using Dominio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicacionWeb.Controllers
+{
+    public class VuelosController : Controller
+    {
+        private Sistema _sistema = Sistema.Instancia;
+        public IActionResult Index()
+        {
+            return View(this._sistema.Vuelos);
+        }
+
+        [HttpGet]
+        public IActionResult Buscar(string codigo)
+        {
+            if (string.IsNullOrWhiteSpace(codigo))
+            {
+                return View(new List<Vuelo>());
+            }
+
+            List<Vuelo> vuelosFiltrados = this._sistema.ListarVuelosPorAeropuerto(codigo);
+
+            if (vuelosFiltrados.Count == 0)
+            {
+                ViewBag.MensajeSinResultados = "No se encontraron vuelos con los datos ingresados.";
+            }
+
+            return View(vuelosFiltrados);
+        }
+    }
+}
---
+++ Dominio/Usuario.cs
@@ -0,0 +1,49 @@
+﻿using Dominio.Interfaces;
+
+namespace Dominio
+{
+    public abstract class Usuario : IValidable
+    {
+        private string _correo;
+        private string _password;
+
+        public string Correo
+        {
+            get { return _correo; }
+        }
+
+        public void Validar()
+        {
+            this.ValidarCorreo();
+            this.ValidarPassword();
+        }
+
+        private void ValidarCorreo()
+        {
+            if (string.IsNullOrWhiteSpace(this._correo))
+            {
+                throw new Exception("El correo no puede estar vacío");
+            }
+
+            if (!this._correo.Contains("@"))
+            {
+                throw new Exception("El correo debe contener el caracter '@'");
+            }
+        }
+
+        private void ValidarPassword()
+        {
+            if (string.IsNullOrWhiteSpace(this._password))
+            {
+                throw new Exception("La contraseña no puede estar vacía");
+            }
+        }
+
+        public Usuario(string correo,
+         string password)
+        {
+            this._correo = correo;
+            this._password = password;
+        }
+    }
+}
