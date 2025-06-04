55b64c03d12db16db1b1013f9c471530a614e87a
ayelenferrer <102334437+ayelenferrer@users.noreply.github.com>
2025-06-03 21:09:35 -0300

    Actualización 03/06

---
+++ Dominio/NewFolder/OrdenPasajes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.NewFolder
+{
+    public class OrdenPasajes : IComparer<Pasaje>
+    {
+        public int Compare(Pasaje? este, Pasaje? otro)
+        {
+            return este.Fecha.CompareTo(otro.Fecha) * -1;
+        }
+    }
+}
